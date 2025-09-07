35f1cd5a3e3ba4aef62d5885e5efb7db38a0e434
Raissa <raissaassissantos7@gmail.com>
2025-09-07 02:24:36 -0300

    Build: setting server

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/livros", new LivroHandler());
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor rodando em http://localhost:8000/livros");
+    }
+}
+
+class LivroHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        if ("GET".equalsIgnoreCase(method)) {
+            String response = "[{\"id\":1,\"titulo\":\"Dom Casmurro\"}]";
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } else {
+            exchange.sendResponseHeaders(405, -1);
+        }
     }
 }
