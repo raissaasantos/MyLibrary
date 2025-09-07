bfaa3b24378fecedbc7cf9fcf6edc10971b17e66
Raissa <raissaassissantos7@gmail.com>
2025-09-07 02:46:53 -0300

    Build: Project structure

--- src/java/com/libraryapp/handler/LivroHandler.java
+++ src/java/com/libraryapp/handler/LivroHandler.java
@@ -1,23 +1,12 @@
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
+package java.com.libraryapp.handler;
+
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-
-        server.createContext("/livros", new LivroHandler());
-
-        server.setExecutor(null);
-        server.start();
-        System.out.println("Servidor rodando em http://localhost:8000/livros");
-    }
-}
-
-class LivroHandler implements HttpHandler {
+public class LivroHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
